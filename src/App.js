f0b107f1bc2e3f673b803b1cb06b2bebe19f4f72
nikhilyadavvvv <nikhilyadavvvv@gmail.com>
2023-03-15 12:54:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.scss";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <h1>Using Sass in React</h1>
+      <header className="wrapper__btns">
+        <button className="btn-blue">Blue Button</button>
+        <button className="btn-red">Red Button</button>
+        <button className="btn-green">Green Button</button>
       </header>
     </div>
   );
 }
-
 export default App;
